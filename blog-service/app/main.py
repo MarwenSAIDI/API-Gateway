494c103b383a8f26e549ad6678e10091c634458d
MarwenSAIDI <marouene.saidi.ds@gmail.com>
2026-06-27 10:29:18 +0100

    Removed the /api prefix from the blog service and the user service

--- blog-service/app/main.py
+++ blog-service/app/main.py
@@ -3,7 +3,7 @@ from app.routes.blog import router
 
 def create_app():
     app_ = FastAPI(title="Blog Service")
-    app_.include_router(router, prefix='/api')
+    app_.include_router(router)
 
     return app_
 
--- user-service/app/main.py
+++ user-service/app/main.py
@@ -3,7 +3,7 @@ from app.routes.user import router
 
 def create_app():
     app_ = FastAPI(title="User Service")
-    app_.include_router(router, prefix='/api')
+    app_.include_router(router)
 
     return app_
 
